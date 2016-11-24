1235955edf07bb3815628200cfd247057b91be19
Xiao-Ou Zhang <kepbod@gmail.com>
2016-11-23 20:49:02 -0500

    fix path

--- snoPlowPy/tests/conftest.py
+++ snoPlowPy/tests/conftest.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.fixture
 def remote_f():
-    return ('https://raw.githubusercontent.com/kepbod/' +
+    return ('https://raw.githubusercontent.com/weng-lab/' +
             'SnoPlowPy/master/snoPlowPy/tests/data/a')
